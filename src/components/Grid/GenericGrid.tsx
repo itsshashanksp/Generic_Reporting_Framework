88645637673996fcc1e90441ff62b0cc6898d967
itsshashanksp <9945shashank@gmail.com>
2026-07-31 17:21:33 +0530

    test

--- src/components/Grid/GenericGrid.tsx
+++ src/components/Grid/GenericGrid.tsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import type { SortChangedEvent } from "ag-grid-community";
 
 import { AgGridReact } from "ag-grid-react";
 
@@ -20,17 +21,40 @@ interface Props {
     rows: any[];
     columns: ColumnDefinition[];
     gridConfig: GridConfig;
+
+    onSortChange?: (
+        sort: {
+            column: string;
+            direction: "ASC" | "DESC";
+        }[]
+    ) => void;
 }
 
 export default function GenericGrid({
     rows,
     columns,
     gridConfig,
+    onSortChange,
 }: Props) {
 
     const { setApi } = useGrid();
 
     const gridRef = useRef<AgGridReact>(null);
+
+    const handleSortChanged = (event: SortChangedEvent) => {
+
+    const sortModel = event.api
+        .getColumnState()
+        .filter(col => col.sort)
+        .map(col => ({
+            column: col.colId,
+            direction: col.sort?.toUpperCase() as "ASC" | "DESC",
+        }));
+
+    console.log("Sort Model", sortModel);
+    onSortChange?.(sortModel);
+
+};
 
     const columnDefs = columns
         .filter(column => column.visible)
@@ -40,6 +64,7 @@ export default function GenericGrid({
             sortable: column.sortable,
             filter: false,
             width: column.width,
+            sortingOrder: ["asc", "desc", null],
         }));
 
     return (
@@ -60,6 +85,8 @@ export default function GenericGrid({
                 columnDefs={columnDefs}
 
                 defaultColDef={defaultColumn}
+
+                onSortChanged={handleSortChanged}
 
                 pagination={gridConfig.pagination}
                 paginationPageSize={gridConfig.pageSize}
